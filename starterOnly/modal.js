f478455829d446f936a50f5ebceadb563899a4d8
DylanLaptop <dylannicolet@hotmail.com>
2021-07-02 19:40:07 +0200

    Location radio validation done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,6 +20,8 @@ const lastNameInput = document.getElementById("last");
 const lastNameDiv = document.getElementById("last-name");
 const emailInput = document.getElementById("email");
 const emailDiv = document.getElementById("email-div");
+const locationDiv = document.getElementById("location-div");
+const checkboxOneInput = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -34,14 +36,32 @@ modalCloseButton.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
 
-//Control if a radio button is selected
-
-
 //To test Email validity (returns true or false)
 function validateEmail(emailAddress){      
   let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailPattern.test(emailAddress);
-} 
+}
+
+//Control if a radio button is selected
+function validateLocationCheckbox(){
+  const checkboxs = document.getElementsByName("location");
+  for(let i=0; i<checkboxs.length; i++){
+    if(checkboxs[i].checked){
+      return true;
+    } else{
+      return false;
+    }
+  }
+};
+
+//Validate Terms and Conditions
+function validateTermsAndConditions(){
+ if (checkboxOneInput.checked){
+   return true;
+ } else{
+   return false;
+ }
+};
 
 // validate modal form user input
 submitButton.addEventListener("click", () => {
@@ -58,5 +78,13 @@ submitButton.addEventListener("click", () => {
   if (validateEmail(emailInput.value) === false) {
     emailDiv.setAttribute("data-error-visible", "true");
     emailDiv.setAttribute("data-error", "Invalid Email Address");
+  }
+
+  if (validateLocationCheckbox() === false){
+    locationDiv.setAttribute("data-error-visible", "true");
+    locationDiv.setAttribute("data-error", "Please select a location");
+  }
+
+  if (validateTermsAndConditions === false){
   }
 });
